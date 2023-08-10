ffb99e0a7826d59ad1c2a86ecfc3a0e49236a350
Sneha Dilip <smidil.eim.ae@tamu.edu>
2023-08-10 23:35:19 +0400

    First run cookies

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 
 app.set('view engine', 'ejs');
@@ -18,9 +21,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-
-
 app.use(express.static("public"));
+
+app.use(session({
+    secret: "This is my huge secret.",
+    resave: false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 
 // creating schema
 const userSchema = new mongoose.Schema({
@@ -31,10 +43,15 @@ const userSchema = new mongoose.Schema({
 //encryption
 
 // userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});   // encryptedfield - only encrypts password tab and not the email tab
-
+userSchema.plugin(passportLocalMongoose); // hash+salt and save users in database
 
 const User = mongoose.model("User",userSchema);
-   
+
+
+passport.use(User.createStrategy());
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/",function(req,res){
@@ -51,39 +68,13 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email:req.body.username,
-            password: hash // md5 > turns password into irreversible hash
-        });
-    
-        newUser.save().then(savedDoc => {
-            savedDoc === newUser; // true
-          });
-          
-        res.render("secrets");
-    });
+
 
   
 });
 
 app.post("/login",function(req,res){
-   const username = req.body.username;
-   const password = req.body.password;
    
-   
-   User.findOne({'email':username})
-   .then(function (userFound) {
-        bcrypt.compare(password, userFound.password, function(err, result) {
-                if(result==true){
-                    res.render("secrets");
-                }
-            });
-   })
-   .catch(function (err) {
-       console.log(err);
-       res.send("Did not find user");
-    })
 });
 
 app.listen(3000, function() {
